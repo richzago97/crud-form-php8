8116277888ac5d4cde6cb4522570da45f1495d5f
richzago <rich.czago@gmail.com>
2023-04-21 11:02:39 -0300

    feat: Created the delete page

---
+++ delete.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+ob_start();
+//Receber o id da URL
+$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+
+//Verificar se o ID possui valor
+if(!empty($id)) {
+   //Incluir os arquivos
+    require "./Conn.php";
+    require "./User.php";
+
+    //Instanciar a classe e criar o objeto
+    $delete = new User();
+
+    //Enviar o id para o atributo da classe
+    $deleteUser->id = $id;
+
+    //Instanciar o método apagar
+    $value = $deleteUser->delete();
+
+    if($value){
+        $_SESSION["msg"] =  "<p style='color: 
+        ##7FFF00;'>Erro: Usuário apagado com sucesso!</p>";
+        header("Location: index.php");
+    }else{
+        $_SESSION["msg"] =  "<p style='color: 
+        #f00;'>Erro: Ocorreu um erro ao tentar deletar o usuário!</p>";
+        header("Location: index.php");
+    }
+
+}else{
+    $_SESSION["msg"] =  "<p style='color: 
+    #f00;'>Erro: Usuário não encontrado!</p>";
+            header("Location: index.php");
+}
+
