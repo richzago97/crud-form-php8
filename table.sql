2023fc78315e5515b17de86be527563f64da5db2
richzago <rich.czago@gmail.com>
2023-04-21 13:28:39 -0300

    fix: Fixed query name that was wrong

--- table.sql
+++ table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(220) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(220) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created` datetime NOT NULL,
-  `modified` datetime DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
